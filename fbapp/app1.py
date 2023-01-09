f8244ddbc710a003d110371d307e0632675c1ff5
trmtsk <53746664+trmtsk@users.noreply.github.com>
2023-01-09 18:15:58 +0900

    230109_modified

--- fbapp/app1.py
+++ fbapp/app1.py
@@ -84,15 +84,16 @@ df["decrease"] = -df["change"].mask(df["change"] > -4, 0)
 df = df.assign(
     RRI=lambda df: (60 / df["value"] * 1000).round(2)
 )
-
+'''
 lfhf_res = df.assign(
         timestamp=lambda df: pd.to_datetime(df["time"])
     ).set_index("timestamp")["RRI"].resample('15min')
-
-#print(td.nni_parameters(df["RRI"]),"\n", type(lfhf_res))
-
-# LF/HF
+lfhf_res.to_csv(f'./tmp/{user_id}_{DATE}.csv')
+print(td.nni_parameters(df["RRI"]),"\n", type(lfhf_res))
 '''
+
+'''
+# LF/HF
 lfhf_res = df.resample('5min').apply(
         lambda x: fd.ar_psd(nni=x, mode='dev')[0]['ar_ratio']
     )
@@ -105,18 +106,19 @@ df["sdnn"] = np.nan
 df["rmssd"] = np.nan
 df["hf"] = np.nan
 #df["mean_bpm"] = np.nan 
-slc = 100
+slc = 50
 itr = int(len(df["RRI"]) / slc) - 1
-#itr2 = int(len(df["value"])/6) - 1
 
+#itr2 = int(len(df["value"])/6) - 1
 #for i in range(5, itr2, 3):
 #    df.at[i, "mean_bpm"] = np.mean(df["value"][i-6:i])
 
 for i in range(itr):
     #results = fd.ar_psd(nni = df["RRI"][slc*i:slc*(i+1)])
     #df.at[slc*(i+1), "ratio"] = results['ar_ratio']
-    results = fd.welch_psd(nni = df["RRI"][slc*i:slc*(i+1)])
+    results = fd.welch_psd(nni = df["RRI"][slc*i:slc*(i+1)], show = False)
     df.at[slc*(i+1), "ratio"] = results['fft_ratio']
+    df.at[slc*(i+1), "hf"] = results['fft_abs'][1]
     sdnn = td.sdnn(nni = df["RRI"][slc*i:slc*(i+1)])
     df.at[slc*(i+1), "sdnn"] = float(sdnn[0])
     rmssd = td.rmssd(nni = df["RRI"][slc*i:slc*(i+1)])
@@ -126,24 +128,24 @@ for i in range(itr):
 #print(results['ar_resampling_frequency'])
 #print(type(df["time"][0]))
 
+#CSV
 df.to_csv(f'./CSV/{user_id}_{DATE}.csv')
 
 df2 = df.dropna()
-df2.to_csv(f'./CSV/{user_id}_{DATE}_dropna.csv')
+df2.to_csv(f'./CSV_dropna/{user_id}_{DATE}_dropna.csv')
 
-#LF/HF
+#HF
 x_label = np.arange(0, len(df2))
 fig, ax = plt.subplots(figsize=(30,10))
 ax.set_title(DATE + "   " + user_id)
 ax.set_xlabel("time")
-ax.set_ylabel("lf/hf")
-ax.plot(df2["time"], df2["ratio"], '-o', label = "LF/HF")
+ax.set_ylabel("hf")
+ax.plot(df2["time"], df2["hf"], '-o', label = "HF")
 plt.xticks(x_label[::3], df2["time"][::3], rotation=60)
 plt.legend(loc = 'best')
-plt.savefig(f'./HF/{user_id}_{DATE}_LFHF.png')
-plt.show()
+plt.savefig(f'./HF/{user_id}_{DATE}_HF.png')
+#plt.show()
 
-'''
 #BPM
 steps = 300
 x_label = np.arange(0, len(df))
@@ -154,8 +156,8 @@ ax.set_ylabel("heart-rate")
 ax.plot(df["time"], df["value"], label = "BPM")
 plt.xticks(x_label[::steps], df["time"][::steps], rotation=60)
 plt.legend(loc = 'best')
-plt.savefig(f'./PNG/{user_id}_{DATE}_BPM.png')
-plt.show()
+plt.savefig(f'./BPM/{user_id}_{DATE}_BPM.png')
+#plt.show()
 
 #LF/HF
 x_label = np.arange(0, len(df2))
@@ -166,8 +168,8 @@ ax.set_ylabel("lf/hf")
 ax.plot(df2["time"], df2["ratio"], '-o', label = "LF/HF")
 plt.xticks(x_label[::3], df2["time"][::3], rotation=60)
 plt.legend(loc = 'best')
-plt.savefig(f'./PNG/{user_id}_{DATE}_LFHF.png')
-plt.show()
+plt.savefig(f'./LFHF/{user_id}_{DATE}_LFHF.png')
+#plt.show()
 
 #SDNN
 x_label = np.arange(0, len(df2))
@@ -178,8 +180,8 @@ ax.set_ylabel("sndd")
 ax.plot(df2["time"], df2["sdnn"], '-o', label = "SDNN")
 plt.xticks(x_label[::3], df2["time"][::3], rotation=60)
 plt.legend(loc = 'best')
-plt.savefig(f'./PNG/{user_id}_{DATE}_SDNN.png')
-plt.show()
+plt.savefig(f'./SDNN/{user_id}_{DATE}_SDNN.png')
+#plt.show()
 
 #rMSSD
 x_label = np.arange(0, len(df2))
@@ -190,9 +192,10 @@ ax.set_ylabel("rmssd")
 ax.plot(df2["time"], df2["rmssd"], '-o', label = "rMSSD")
 plt.xticks(x_label[::3], df2["time"][::3], rotation=60)
 plt.legend(loc = 'best')
-plt.savefig(f'./PNG/{user_id}_{DATE}_rMSSD.png')
-plt.show()
+plt.savefig(f'./rMSSD/{user_id}_{DATE}_rMSSD.png')
+#plt.show()
 
+'''
 #BPM_mean
 x_label = np.arange(0, len(df2))
 fig, ax = plt.subplots(figsize=(30,10))
@@ -203,5 +206,5 @@ ax.plot(df2["time"], df2["rmssd"], '-o', label = "rMSSD")
 plt.xticks(x_label[::3], df2["time"][::3], rotation=60)
 plt.legend(loc = 'best')
 plt.savefig(f'/content/drive/MyDrive/研究/Data/PNG/{user_id}_{DATE}_rMSSD.png')
-plt.show()
+#plt.show()
 '''
